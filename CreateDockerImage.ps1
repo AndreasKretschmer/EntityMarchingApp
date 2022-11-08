39945c0f82b3ae6976203197820467c4ad83cfc2
Andreas Kretschmer <andy.kretschmer@web.de>
2022-11-08 20:46:15 +0100

    Initial Commit

---
+++ CreateDockerImage.ps1
@@ -0,0 +1,20 @@
+﻿install-module bccontainerhelper -Force
+Get-InstalledModule
+
+$containerName = 'bc'
+$password = 'P@ssw0rd'
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object pscredential 'admin', $securePassword
+$auth = 'UserPassword'
+$artifactUrl = Get-BcArtifactUrl -type 'Sandbox' -country 'de' -select 'Latest'
+$licenseFile = 'C:\Users\KRAN\Desktop\5306956.bclicense'
+New-BcContainer `
+    -accept_eula `
+    -containerName $containerName `
+    -credential $credential `
+    -auth $auth `
+    -artifactUrl $artifactUrl `
+    -imageName 'entitymatching' `
+    -licenseFile $licenseFile ``
+    -memoryLimit 8G `
+    -updateHosts
