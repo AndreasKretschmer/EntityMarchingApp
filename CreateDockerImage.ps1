0c51f1483316ab4b345ff48de0c167e168d87acd
Andreas Kretschmer <andy.kretschmer@web.de>
2024-04-08 20:16:22 +0200

    updated dockker script

--- CreateDockerImage.ps1
+++ CreateDockerImage.ps1
@@ -1,19 +1,19 @@
-﻿install-module bccontainerhelper -Force
-Get-InstalledModule
-
-$containerName = 'entitymatching'
-$password = 'P@ssw0rd'
+﻿$containerName = 'entitymatching'
+$password = '#########'
 $securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 $credential = New-Object pscredential 'admin', $securePassword
 $auth = 'UserPassword'
 $artifactUrl = Get-BcArtifactUrl -type 'Sandbox' -country 'de' -select 'Latest'
-$licenseFile = 'C:\Users\KRAN\Desktop\5306956.bclicense'
 New-BcContainer `
     -accept_eula `
     -containerName $containerName `
     -credential $credential `
     -auth $auth `
     -artifactUrl $artifactUrl `
-    -imageName 'entitymatching' `
-    -licenseFile $licenseFile ``
-    -memoryLimit 8G
+    -imageName 'mybcimage' `
+    -multitenant:$false `
+    -includeTestToolkit `
+    -includeTestLibrariesOnly `
+    -includePerformanceToolkit `
+    -dns '8.8.8.8' `
+    -updateHosts
